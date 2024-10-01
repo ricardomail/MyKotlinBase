92e2731178e0038ab203b2a6bc46eddd2052643c
oasis <ricardomail@163.com>
2024-10-01 14:10:43 +0800

    feat: 函数

--- app/src/main/java/com/oasis/mykotlinbase/Base03.kt
+++ app/src/main/java/com/oasis/mykotlinbase/Base03.kt
@@ -2,7 +2,20 @@ package com.oasis.mykotlinbase
 
 
 fun main() {
-    funcParams()
+//    funcParams()
+//    loginApi("zhangsan", "") { error, code ->
+//        println("$error $code")
+//    }
+    // 也可以这么写，但是函数参数在最后时可以放到括号外面
+//    loginApi("zhangsan", "123", { error, code ->
+//        println("$error $code")
+//    })
+
+    // 函数引用 ::会将函数变成函数类型的对象
+    login("", "", ::responseMethod)
+    var anonymous = action03()
+    anonymous.invoke("hello")
+    anonymous.invoke("123")
 }
 
 // 匿名函数
@@ -37,5 +50,54 @@ private fun funcParams() {
         // 如果只有一个参数，默认会增加it作为参数的调用
         println("$it")
         "$it"
+    }
+}
+
+// 匿名函数的类型推断
+private fun funcAnonymous() {
+    // 不需要指定匿名函数的参数类型和返回值类型，会根据方法内的参数和返回值自动推断
+
+    val methodAction = { a: Int, b: Int ->
+        println("$a, $b")
+        ""
+    } // 相当于函数(Int, Int)->String
+
+    methodAction(1, 2)
+}
+
+// lambda
+private fun funcLambda() {
+    // 匿名函数等价于lambda表达式
+}
+
+// 函数的参数为函数
+inline fun loginApi(username: String, pwd: String, response: (String, Int) -> Unit) {
+    if (username.isEmpty() || pwd.isEmpty()) {
+        println("error")
+        response.invoke("error", 500)
+        return
+    }
+    response.invoke("success", 200)
+}
+
+// 内联 函数使用lambda函数最好声明称内联，如果未使用内联，在调用端会生成多个对象，造成性能损耗
+// 内联会讲代码替换到调用处，减少性能损耗
+
+
+//函数引用
+inline fun login(name: String, pwd: String, response: (String, Int) -> Unit) {
+
+}
+
+fun responseMethod(message: String, code: Int) {
+
+}
+
+// 函数作为返回值
+private fun action03(): (String) -> Unit {
+    val num = 100
+    return { result ->
+        println(num)
+        println(result)
     }
 }
